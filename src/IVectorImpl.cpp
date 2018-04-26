5363edc1c94d4a9ae5b6d258c92fe6d717d4fa15
grigoriy <gri-bel@yandex.ru>
2018-04-26 22:17:54 +0300

    added something new to IVrctorImpl

--- src/IVectorImpl.cpp
+++ src/IVectorImpl.cpp
@@ -23,3 +23,106 @@ IVector::vector_ptr IVectorImpl::copyVector(IVectorImpl const& v){
 IVector::vector_ptr IVectorImpl::moveVector(IVectorImpl &&v) {
     return QSharedPointer<IVector>(new IVectorImpl(std::move(v)));
 }
+
+IVector::ReturnCode IVectorImpl::add(IVector const & added)
+{
+	IVector::ReturnCode error = NO_ERRORS;
+	if (this->dim != added.getDimension())
+	{
+		error = DIFFERENT_DIMENSIONS;
+	}
+	for (int i = 0; i < this->dim; i++)
+	{
+		double tmp = 0;
+		error = added.getElement(i, tmp);
+		if (error != NO_ERRORS)
+			break;
+		this->elem[i] += tmp;
+	}
+	return error;
+}
+
+IVector::ReturnCode IVectorImpl::substract(IVector const & substracted)
+{
+	IVector::ReturnCode error = NO_ERRORS;
+	if (this->dim != substracted.getDimension())
+	{
+		error = DIFFERENT_DIMENSIONS;
+	}
+	for (int i = 0; i < this->dim; i++)
+	{
+		double tmp = 0;
+		error = substracted.getElement(i, tmp);
+		if (error != NO_ERRORS)
+			break;
+		this->elem[i] -= tmp;
+	}
+	return error;
+}
+
+IVector::ReturnCode IVectorImpl::dotProduct(IVector const & multiplier)
+{
+	IVector::ReturnCode error = NO_ERRORS;
+	if (this->dim != multiplier.getDimension())
+	{
+		error = DIFFERENT_DIMENSIONS;
+	}
+	for (int i = 0; i < this->dim; i++)
+	{
+		double tmp = 0;
+		error = multiplier.getElement(i, tmp);
+		if (error != NO_ERRORS)
+			break;
+		this->elem[i] *= tmp;
+	}
+	return error;
+}
+
+IVector::ReturnCode IVectorImpl::crossProduct(IVector const & right)
+{
+	return IVector::ReturnCode();
+}
+
+IVector::ReturnCode IVectorImpl::multiplyByScalar(double multipler)
+{
+	IVector::ReturnCode error = NO_ERRORS;
+	for (int i = 0; i < this->dim; i++)
+	{
+		this->elem[i] *= multipler;
+	}
+	return error;
+}
+
+IVector::ReturnCode IVectorImpl::getElement(size_t index, double & elem) const
+{
+	IVector::ReturnCode error = NO_ERRORS;
+	if (index > this->dim)
+	{
+		error = OUT_OF_RANGE;
+	}
+	elem = this->elem[index];
+	return error;
+}
+
+IVector::ReturnCode IVectorImpl::setElement(size_t index, double elem)
+{
+	IVector::ReturnCode error = NO_ERRORS;
+	if (index > this->dim)
+	{
+		error = OUT_OF_RANGE;
+	}
+	this->elem[index] = elem;
+	return error;
+}
+
+IVector::ReturnCode IVectorImpl::setVector(IVector const & v)
+{
+	return IVector::ReturnCode();
+}
+
+QSharedPointer<Logger> IVectorImpl::getLogger() const
+{
+	return QSharedPointer<Logger>();
+}
+
+
