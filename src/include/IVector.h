d1d0bb2cbc8b80d06fb8984dec91a0c046b9b258
AlisaMironenko <alic98@yandex.ru>
2018-04-30 19:12:22 +0300

    Updated vector interface

--- src/include/IVector.h
+++ src/include/IVector.h
@@ -2,47 +2,24 @@
 #define IVECTOR_H
 #include<cmath>
 #include <QSharedPointer>
+#include "opt.h"
 
-namespace opt {
-
-class Logger;
-
-enum ReturnCode {
-    NO_ERRORS,
-    DIFFERENT_DIMENSIONS,
-    MEMORY_ALLOCATION,
-    OUT_OF_RANGE
-};
-
-enum TypeNorm {
-    NORM1,
-    NORM2
-};
-}
 
 class IVector{
 public:
-	using vector_ptr = QSharedPointer<IVector>;
-
-    static vector_ptr createVector(opt::Logger const& l);
-    static vector_ptr moveVector(IVector&& v);
-    static vector_ptr copyVector(IVector const& v);
+    static IVector* createVector();
+    static IVector* copyVector(QSharedPointer<IVector>& v);
 
     virtual opt::ReturnCode add(IVector const& added) = 0;
     virtual opt::ReturnCode substract(IVector const& substracted) = 0;
     virtual opt::ReturnCode dotProduct(IVector const& multiplier) = 0;
-    //virtual opt::ReturnCode crossProduct(IVector const& right) = 0;
     virtual opt::ReturnCode multiplyByScalar(double multipler) = 0;
 
-    //virtual opt::ReturnCode setDimension(size_t dim) = 0;
     virtual size_t getDimension() const = 0;
     virtual opt::ReturnCode getElement(size_t index, double& elem) const = 0;
     virtual opt::ReturnCode setElement(size_t index, double elem) = 0;
     virtual opt::ReturnCode setVector(IVector const& v) = 0;
-    //virtual double const* getPtr() const = 0;
-    //virtual QSharedPointer<opt::Logger> getLogger() const = 0;
-    //virtual void setLogger(opt::Logger const& l) = 0;
-	//TYPE_ID get_type_info()
+    virtual double const* getPtr() const = 0;
 
     virtual double norm(opt::TypeNorm t) const = 0;
     virtual bool GT(opt::TypeNorm t, IVector const& v) const = 0;
--- src/include/IVectorImpl.h
+++ src/include/IVectorImpl.h
@@ -19,22 +19,18 @@
 
 class IVectorImpl : public virtual IVector{
 public:
-    static IVector::vector_ptr createVector(opt::Logger const& l);
-    static IVector::vector_ptr moveVector(IVectorImpl&& v);
-    static IVector::vector_ptr copyVector(const IVectorImpl& v);
+    static IVector* createVector(opt::Logger const& l);
+    static IVector* copyVector(const IVectorImpl& v);
 
     opt::ReturnCode add(IVector const& added) override;
     opt::ReturnCode substract(IVector const& substracted) override;
     opt::ReturnCode dotProduct(IVector const& multiplier) override;
-    //opt::ReturnCode crossProduct(IVector const& right) override;
     opt::ReturnCode multiplyByScalar(double multipler) override;
 
-    //opt::ReturnCode setDimension(size_t dim); //пока opt::ReturnCode, далее - opt::ReturnCode_CODE
     size_t getDimension() const override { return _dim; }
     opt::ReturnCode getElement(size_t index, double& elem) const override;
     opt::ReturnCode setElement(size_t index, double elem) override;
     opt::ReturnCode setVector(IVector const& v) override;
-    //inline QSharedPointer<opt::Logger> getLogger() const override;
 
     double norm(opt::TypeNorm t) const override;
     bool GT(opt::TypeNorm t, IVector const& v) const override;
@@ -43,12 +39,7 @@ public:
 
     ~IVectorImpl() override {}
 protected:
-    //IVectorImpl(opt::Logger const& l);
     IVectorImpl(IVectorImpl const& v);
-    /*IVectorImpl(IVectorImpl&& v)
-        : _dim(std::move(v._dim),
-               _vals(v._vals),
-               _l(std::move(v._l)) {};*/
 private:
     inline double _norm_l1() const;
     inline double _norm_l2() const;
